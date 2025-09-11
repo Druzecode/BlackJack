1ba93f8c1bd41611669dbe564ce3baadc7376104
SimpleHealthcareApps <info@simplehealthcareapps.com>
2025-09-11 15:51:19 -0700

    Removed additional unused dependencies and things not supported by the TI platform.

--- PythonBlackjack/Blkjack.py
+++ PythonBlackjack/Blkjack.py
@@ -1,30 +1,9 @@
-
-import random
-import time
-import os
-
-from Card import Card
 from Deck import Deck
 from Game import Game
-from Player import Player
-from DispLg import DispLg
 from DispSm import DispSm
 
-
-
 def main():
-
-    try:
-        size = os.get_terminal_size()
-        columns = size.columns
-        rows = size.lines
-    except OSError:
-        columns = 80  # default width
-        rows = 24     # default height
-
-    #print(f"Columns: {columns}, Rows: {rows}")
-
-    display = DispSm() if columns < 50 else DispLg()
+    display = DispSm()
     display.show_intro()
     deck = Deck()
     deck.shuffle()
